ae8a34891c0c11739b653ce1ac76be1ac0dcb3aa
Nick Wang <nickyiliwang@gmail.com>
2019-05-16 17:26:37 -0400

    WIP 25/29, break in dicator not working

--- script.js
+++ script.js
@@ -1,135 +1,120 @@
-//Length
 let sessionLength = document.querySelector("#session-length");
 let breakLength = document.querySelector("#break-length");
-//Increment
 let breakInc = document.querySelector("#break-increment");
 let sessionInc = document.querySelector("#session-increment");
-//Decrement
 let breakDec = document.querySelector("#break-decrement");
 let sessionDec = document.querySelector("#session-decrement");
-//display
 let display = document.querySelector("#time-left");
-//labels
 let label = document.querySelector("#timer-label");
-let breakLabel = document.querySelector("#break-length-label");
-let sessionLabel = document.querySelector("#session-length-label");
-//start/stop
 let startStop = document.querySelector("#start_stop");
-//Reset
 let reset = document.querySelector("#reset");
-//audio
 let audio = document.querySelector("#beep");
-//Bool Check
-let isPaused = true;
-let isSession = true;
-let isBegin = false;
-//Time values
-let seconds = 0;
-let minutes = 0;
-///////////////////////////////////////////////////////////////////////////////////
 
 //Increment click listeners
 breakInc.addEventListener("click", () => {
-  breakLength.value < 60 ? breakLength.value++ : breakLength.value + 0;
-  breakLabel.textContent = "Break Length: " + breakLength.value;
+  if (breakLength.value < 60) {
+    breakLength.value++;
+  }
 });
 sessionInc.addEventListener("click", () => {
-  sessionLength.value < 60 ? sessionLength.value++ : sessionLength.value + 0;
-  sessionLabel.textContent = "Session Length: " + sessionLength.value;
+  if (sessionLength.value < 60) {
+    sessionLength.value++;
+  }
 });
 //Decrement click listeners
 breakDec.addEventListener("click", () => {
-  breakLength.value > 1 ? breakLength.value-- : breakLength.value + 0;
-  breakLabel.textContent = "Break Length: " + breakLength.value;
+  if (breakLength.value > 1) {
+    breakLength.value--;
+  }
 });
 sessionDec.addEventListener("click", () => {
-  sessionLength.value > 1 ? sessionLength.value-- : sessionLength.value + 0;
-  sessionLabel.textContent = "Session Length: " + sessionLength.value;
-});
-///////////////////////////////////////////////////////////////////////////////////
-
-//start stop
-startStop.addEventListener("click", () => {
-  isSession === true
-    ? (label.textContent = "Session is initialized")
-    : (label.textContent = "Break is initialized");
-  isBegin = true;
-  isPaused === false ? (isPaused = true) : (isPaused = false);
-});
-
-//reset
-reset.addEventListener("click", () => {
-  isPaused = true;
-  isSession = true;
-  isBegin = false;
-  sessionLength.value = 25;
-  breakLength.value = 5;
-  minutes = sessionLength.value.slice();
-  seconds = 0;
-  label.textContent = "Session is initialized";
-  audio.load();
-});
-
-//Break or Session?
-const breakOrSession = () => {
-  //if it isn't break, minutes is session val
-  if (isSession === true) {
-    label.textContent = "Break is initialized";
-    minutes = breakLength.value.slice();
-    isSession = false;
-    isPaused = false;
-  } else if (isSession === false) {
-    label.textContent = "Session is initialized";
-    minutes = sessionLength.value.slice();
-    isSession = true;
-    isPaused = false;
+  if (sessionLength.value > 1) {
+    sessionLength.value--;
   }
-};
-//display
+});
+
+//Bool Check
+let isPaused = true;
+let isSession = true;
+let isRunning = false;
+//Time values
+let seconds = 0;
+let minutes = sessionLength.value;
+
 const displayElements = () => {
-  if (minutes < 10 && seconds > 10) {
+  if (minutes < 10 && seconds >= 10) {
     display.textContent =
       minutes.toString().padStart(2, "0") + ":" + seconds.toString();
   }
-
   if (minutes < 10 && seconds < 10) {
     display.textContent =
       minutes.toString().padStart(2, "0") +
       ":" +
       seconds.toString().padStart(2, "0");
   }
-
-  if (minutes > 10 && seconds > 10) {
-    display.textContent = minutes.toString() + ":" + seconds.toString();
-  }
-
-  if (minutes > 10 && seconds < 10) {
+  if (minutes >= 10 && seconds < 10) {
     display.textContent =
       minutes.toString() + ":" + seconds.toString().padStart(2, "0");
+  }
+  if (minutes >= 10 && seconds >= 10) {
+    display.textContent = minutes.toString() + ":" + seconds.toString();
+  }
+};
+
+//Break or Session?
+const breakOrSession = () => {
+  if (isSession === true) {
+    label.textContent = "Session";
+    minutes = sessionLength.value;
+  }
+  if (isSession === false) {
+    label.textContent = "Break";
+    minutes = breakLength.value;
   }
 };
 
 const stopWatch = () => {
-  if (isBegin === false) {
-    seconds = 0;
-    minutes = sessionLength.value.slice();
-  }
-
-  if (seconds > 0 && isPaused === false) {
+  console.log("running");
+  if (seconds > 0 && isPaused === false && isRunning === true) {
     seconds--;
   }
-
-  if (seconds === 0 && minutes > 0 && isBegin === true) {
+  if (seconds === 0 && minutes > 0 && isRunning === true) {
     minutes--;
     seconds = 59;
   }
-
-  if (minutes === 0 && seconds === 0) {
+  if (minutes === 0 && seconds === 0 && isRunning === true) {
     isPaused = true;
     audio.play();
-    breakOrSession();
   }
   displayElements();
 };
 
-setInterval(stopWatch, 0);
+//start stop
+const startTimer = () => {
+  if (isRunning === false) {
+    isRunning = true;
+    isPaused = false;
+    minutes = sessionLength.value;
+    setInterval(stopWatch, 1000);
+  }
+
+  if (isRunning === true) {
+    isPaused === false ? (isPaused = true) : (isPaused = false);
+  }
+};
+
+startStop.addEventListener("click", startTimer);
+
+//reset
+const resetTimer = () => {
+  isPaused = true;
+  isSession = true;
+  isRunning = false;
+  sessionLength.value = 25;
+  breakLength.value = 5;
+  minutes = sessionLength.value;
+  seconds = 0;
+  audio.load();
+};
+
+reset.addEventListener("click", resetTimer);
